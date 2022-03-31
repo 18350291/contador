e3c23fca8640f232d418a070cf98d2163d321a94
Alexis Antonio Arano Castañeda <l18350291@gmail.com>
2022-03-31 09:58:10 -0600

    Segundo commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import {useState} from "react";
+
+function App() {
+
+  const [cuenta, setcuenta] = useState(0)
+
+  const handleClick = () => {
+    setcuenta(cuenta+1);
+  }
+
+  return (
+    <div className="App">
+      <h3>Contador</h3>
+      <hr/>
+        <h2 className="text-center">{cuenta}</h2>
+      <hr/>
+
+      <div 
+        style={{
+          display: "flex", 
+          justifyContent: "flex-end", 
+          marginRight: "25px"
+        }}>
+        <button type="button" className="btn btn-primary" onClick={handleClick}>+1</button>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
