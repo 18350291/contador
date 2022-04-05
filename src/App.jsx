482c8b47976fa135de6587da611c4ca4bd984cd0
Alexis Antonio Arano Castañeda <l18350291@gmail.com>
2022-04-05 10:07:07 -0500

    Agregado del campo para sumatoria

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import {useState} from "react";
 
 function App() {
 
-  const [cuenta, setcuenta] = useState(0)
+  const [cuenta, setcuenta] = useState(0);
+  const [paso, setPaso] = useState(1);
 
   const handleClickSuma = () => {
-    setcuenta(cuenta+1);
+    setcuenta(cuenta+paso);
   }
 
   const handleClickResta = () => {
-    setcuenta(cuenta-1);
+    setcuenta(cuenta-paso);
   }
 
   const handleClickReinicia = () => {
     setcuenta(cuenta-cuenta);
+    setPaso(1);
+  }
+
+  const handleInputChange = (e) => {
+    if(isNaN(e.target.value)){
+      return;
+    }
+      setPaso(Number(e.target.value));
+
   }
 
   return (
@@ -26,12 +36,54 @@ function App() {
       <div 
         style={{ 
           display: "flex", 
-          justifyContent: "center", 
+          justifyContent: "flex-end", 
+          marginRight:"15px"
 
         }}>
-        <button type="button" className="btn btn-primary" onClick={handleClickSuma}>+1</button>
-        <button type="button" className="btn btn-primary" onClick={handleClickResta}>-1</button>
-        <button type="button" className="btn btn-primary" onClick={handleClickReinicia}>Reiniciar</button>
+
+        <label>
+          Paso
+          <input 
+            id="Paso"
+            name="Paso"
+            type="text" 
+            value={paso}
+            onChange={handleInputChange}
+            style={{
+              marginLeft:"15px",
+              width: "60px"
+            }}
+          />
+        </label>
+
+        <button 
+          type="button" 
+          className="btn btn-primary" 
+          onClick={handleClickReinicia}
+          style={{marginLeft:"15px"}}
+        >
+          Reiniciar
+        </button>
+
+        <button 
+          type="button" 
+          className="btn btn-primary" 
+          onClick={handleClickSuma}
+          style={{marginLeft:"15px"}}
+        >
+          +1
+        </button>
+
+        <button 
+          type="button" 
+          className="btn btn-primary" 
+          onClick={handleClickResta}
+          style={{marginLeft:"15px"}}
+        >
+          -1
+        </button>
+
+
 
       </div>
 
